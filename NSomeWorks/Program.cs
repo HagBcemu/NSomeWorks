a0b2ae45c2d1b06174f7cd84e12d7e50c030f40c
HagBcemu <vlad051198vlad@gmail.com>
2022-10-26 14:12:47 +0300

    add .sln

---
+++ NSomeWorks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NSomeWorks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Master");
+        }
+    }
+}
