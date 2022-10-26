3f49fca7836053de348640d2a7232af2b4858c61
HagBcemu <vlad051198vlad@gmail.com>
2022-10-26 17:48:43 +0300

    Develop Module1HW3

---
+++ NSomeWorks/Starter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace NSomeWorks
+{
+    class Starter
+    {
+        Logger _logger;
+
+        Result _result;
+        public void Run()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Actions actions = new Actions();
+                switch (new Random().Next(1, 4))
+                {
+                    case 1:
+                        _result = actions.StartMethod();
+                        break;
+                    case 2:
+                        _result = actions.SkipedMethod();
+                        break;
+                    case 3:
+                        _result = actions.ErrorMethod();
+                        break;
+                }
+
+                _logger = Logger.GetLogger();
+                if (_result.Status == false)
+                {
+                    _logger.ErorLog(_result);
+                }
+                else
+                {
+                    _logger.WriteLogInConsole(_result);
+                }
+
+                Thread.Sleep(100);
+            }
+
+            _logger.WriteLogInFile();
+        }
+    }
+}
