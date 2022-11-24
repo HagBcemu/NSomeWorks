9406e3d26245270b024178df6c4b7b8a2d977de5
HagBcemu <vlad051198vlad@gmail.com>
2022-11-25 00:37:44 +0200

    Add all developed

---
+++ NSomeWorks/ConsoleWriter.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace NSomeWorks
+{
+    class ConsoleWriter
+    {
+        private NoteContact _noteContact;
+
+        private Language _language = Language.English;
+        public ConsoleWriter()
+        {
+            _noteContact = new NoteContact();
+            _noteContact.AddListContact(ContactCreater.GetContacts());
+        }
+
+        public void ShowConsole()
+        {
+            ShowNoteContact();
+            ShowControlPanelNoteContact();
+        }
+
+        void ShowNoteContact()
+        {
+            Console.Clear();
+            Console.WriteLine("Контакты: ");
+            List<GroupContact> groupContact = _noteContact.GetAllSortContact(_language);
+            foreach (var group in groupContact)
+            {
+                Console.WriteLine(group.GroupName + ":_______");
+                foreach (var contact in group.GetListContact())
+                {
+                    Console.WriteLine("\t" + contact.Name + " - " + contact.PhoneNumber);
+                }
+            }
+
+            Console.WriteLine("\nВведите номер команды:");
+            Console.WriteLine("1 - для добавления контакта");
+            Console.WriteLine("2 - для смены языка на Английский");
+            Console.WriteLine("3 - для смены языка на Украинский");
+            Console.WriteLine("4 - для выхода");
+        }
+
+        void ShowControlPanelNoteContact()
+        {
+            while (true)
+            {
+                int numberChose = 0;
+                string consoleWrite = Console.ReadLine();
+                bool succesParseNumber = int.TryParse(consoleWrite, out numberChose);
+                if (succesParseNumber)
+                {
+                    if (numberChose == 1)
+                    {
+                        Console.WriteLine("Введите через /");
+                        string userInput = Console.ReadLine();
+                        string[] inputSlit = userInput.Split("/");
+                        if (inputSlit.Length >= 2)
+                        {
+                            if (inputSlit[0] != string.Empty && inputSlit[1] != string.Empty)
+                            {
+                                _noteContact.AddContact(inputSlit[0], inputSlit[1]);
+                                ShowConsole();
+                            }
+                            else
+                            {
+                                ShowNoteContact();
+                                Console.WriteLine("Что то не так. Введите заново номер команды");
+                            }
+                        }
+                        else
+                        {
+                            ShowNoteContact();
+                            Console.WriteLine("Что то не так. Введите заново номер команды");
+                        }
+                    }
+
+                    if (numberChose == 2)
+                    {
+                        _language = Language.English;
+                        ShowNoteContact();
+                    }
+
+                    if (numberChose == 3)
+                    {
+                        _language = Language.Ukrainian;
+                        ShowNoteContact();
+                    }
+
+                    if (numberChose == 4)
+                    {
+                        Process.GetCurrentProcess().Kill();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Не корректное значение");
+                }
+            }
+        }
+    }
+}
---
+++ NSomeWorks/Contact/GroupContact.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NSomeWorks
+{
+    class GroupContact
+    {
+        private string _groupName;
+
+        private List<Contact> _contacts;
+        public GroupContact()
+        {
+            _contacts = new List<Contact>();
+        }
+
+        public string GroupName
+        {
+            get { return _groupName; }
+            set { _groupName = value; }
+        }
+
+        public List<Contact> GetListContact()
+        {
+            return _contacts;
+        }
+
+        public void AddContact(Contact contact)
+        {
+            _contacts.Add(contact);
+        }
+    }
+}
