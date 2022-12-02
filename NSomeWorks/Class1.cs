65c9eacf46f3aabe85e1863c77d072058eaccb93
HagBcemu <vlad051198vlad@gmail.com>
2022-12-02 16:25:28 +0200

    Add all develoed

---
+++ NSomeWorks/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NSomeWorks
+{
+    class Class1
+    {
+        public delegate void ShowHadler(bool boolStatus);
+
+        public delegate int PowHadler(int x, int y);
+
+        public static int Pow(int x, int y)
+        {
+            return x * y;
+        }
+    }
+}
